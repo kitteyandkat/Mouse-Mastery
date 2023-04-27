6dc02f239a6c94c139c2bf46c521f07e43fed325
Sarah Johnson <kitteyandkat@aim.com>
2023-04-27 18:01:17 -0400

    Basic Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [platform, setPlatform] = useState('pc')
+
+  const updatePlatform = e => setPlatform(e.target.dataset.platform)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='main-container'>
+      {/* <h1> MouseMastery </h1> */}
+      <div className='tutorial-container'>
+        <div className="platform-buttons">
+          <button data-active={platform === 'mac'} onClick={updatePlatform} data-platform="mac">MAC</button>
+          <button data-active={platform === 'windows'} onClick={updatePlatform} data-platform="windows">WINDOWS</button>
+          <button data-active={platform === 'ios'} onClick={updatePlatform} data-platform="ios">iOS</button>
+          <button data-active={platform === 'android'} onClick={updatePlatform} data-platform="android">ANDROID</button>
+        </div>
+        <div className='tutorial-screen'>
+          {/* {platform === 'windows' && <WindowsTutorial/>} */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
