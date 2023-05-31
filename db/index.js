a059145912a3ea3d7c1e1e48d519f9de0c484d34
Sarah Johnson <kitteyandkat@aim.com>
2023-05-31 19:38:24 -0400

    actual build fix

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 import express, { application } from 'express'
 import cors from 'cors'
-import {create, read, update, delety, readWithJoin, readModulesWithSteps} from './queries.js'
+import { create, read, update, delety, readWithJoin, readModulesWithSteps } from './queries.js'
 import { readdir } from 'fs/promises'
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,13 +11,13 @@ app.post('/api/:table', create)
 app.get('/api/getAllModules', readModulesWithSteps)
 app.get('/api/:table', read)
 app.get('/api/:table1/:table2', readWithJoin)
-app.delete('/api/:table/:id',delety)
+app.delete('/api/:table/:id', delety)
 app.put('/api/:table/:id', update)
-app.use(express.static('../client/dist')) 
 app.get('/', async (req, res) => res.json(await readdir('/', { withFileTypes: true }))
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name))
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => dirent.name))
 
+app.use(express.static('../client/dist'))
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
