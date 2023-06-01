53784ba77972f0f5a2ba6aaf56fc1509de24625d
Sarah Johnson <kitteyandkat@aim.com>
2023-05-31 20:49:34 -0400

    remove vite config

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
-import express, { application } from 'express'
+import express from 'express'
 import cors from 'cors'
 import { create, read, update, delety, readWithJoin, readModulesWithSteps } from './queries.js'
-import { readdir } from 'fs/promises'
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -13,11 +14,10 @@ app.get('/api/:table', read)
 app.get('/api/:table1/:table2', readWithJoin)
 app.delete('/api/:table/:id', delety)
 app.put('/api/:table/:id', update)
-app.get('/', async (req, res) => res.json(await readdir('/', { withFileTypes: true }))
-  .filter(dirent => dirent.isDirectory())
-  .map(dirent => dirent.name))
 
-app.use(express.static('../client/dist'))
+
+const directory = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(directory, '..', 'client', 'dist')));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
