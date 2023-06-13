8436f283dfc790f730e3d5bf0cde7eafafc42e75
Hennc94 <h3nn3ssyc@yahoo.com>
2023-06-12 20:30:00 -0400

    styling changes

--- client/src/assets/LoadingPage.jsx
+++ client/src/assets/LoadingPage.jsx
@@ -1,29 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 import './LoadingPage.css';
+
 import androidIcon from './images/androidlogo.png';
 import appleIcon from './images/applelogo.png';
 import windowsIcon from './images/windowslogo.png';
 
 const LoadingPage = () => {
-  const [showNextComponent, setShowNextComponent] = useState(false);
-  const history = useHistory();
-  
-  useEffect(() => {
-    const delay = 3000; // 3 seconds delay
-    const timeoutId = setTimeout(() => {
-      setShowNextComponent(true);
-    }, delay);
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, []);
-
-  const navigateToNextPage = () => {
-    history.push('/home'); // Replace '/home' with the desired path of the next page
-  };
-
   return (
     <div className="loading-page">
       <h1 className="logo">Mouse Mastery</h1>
@@ -45,10 +27,6 @@ const LoadingPage = () => {
         </figure>
       </div>
       <h2>Loading</h2>
-      {showNextComponent && (
-        <button onClick={navigateToNextPage}>Continue</button>
-        // Replace the button with your desired next component or navigation method
-      )}
     </div>
   );
 }
