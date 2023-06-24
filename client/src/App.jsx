51d361fdbfe43fcf8b3a24ec1496e20183a5ed61
Connor Hennessy <115747770+Hennc94@users.noreply.github.com>
2023-06-24 09:47:21 -0400

    new files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,86 @@
-import { useState } from 'react'
-import './App.css'
-import Windows11 from './assets/Windows/Windows'
-import PixelClone from './assets/Android/Pixel'
-import Mac from './assets/Mac/src/Mac'
-import WindowsClone from './assets/Windows/src/App'
-
-import IOSClone from './assets/iOS/ios'
-
+import React, { useState, useEffect } from 'react';
+import LoadingPage from './LoadingPage';
+import LandingPage from './LandingPage';
+import SelectionPage from './SelectionPage';
+import Windows11 from './assets/Windows/Windows';
+import PixelClone from './assets/Android/Pixel';
+import Mac from './assets/Mac/src/Mac';
+import IOSClone from './assets/iOS/ios';
+import './App.css';
 
 function App() {
-  const [platform, setPlatform] = useState('windows')
+  const [isLoading, setIsLoading] = useState(true);
+  const [showStartPage, setShowStartPage] = useState(true);
+  const [showSelectionPage, setShowSelectionPage] = useState(false);
+  const [platform, setPlatform] = useState('windows');
 
-  const updatePlatform = e => setPlatform(e.target.dataset.platform)
+  const updatePlatform = (e) => setPlatform(e.target.dataset.platform);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  }, []);
+
+  const handleStart = () => {
+    setShowStartPage(false);
+    setShowSelectionPage(true);
+  };
+
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+
+  if (showStartPage) {
+    return <LandingPage onStart={handleStart} />;
+  }
+
+  if (showSelectionPage) {
+    return <SelectionPage />;
+  }
 
   return (
-    <div className='main-container'>
-      {/* <h1> MouseMastery </h1> */}
-      <div className='tutorial-container'>
+    <div className="main-container">
+      <div className="tutorial-container">
         <div className="platform-buttons">
-          <button data-active={platform === 'mac'} onClick={updatePlatform} data-platform="mac">MAC</button>
-          <button data-active={platform === 'windows'} onClick={updatePlatform} data-platform="windows">WINDOWS</button>
-          <button data-active={platform === 'ios'} onClick={updatePlatform} data-platform="ios">iOS</button>
-          <button data-active={platform === 'android'} onClick={updatePlatform} data-platform="android">ANDROID</button>
+          <button
+            data-active={platform === 'mac'}
+            onClick={updatePlatform}
+            data-platform="mac"
+          >
+            MAC
+          </button>
+          <button
+            data-active={platform === 'windows'}
+            onClick={updatePlatform}
+            data-platform="windows"
+          >
+            WINDOWS
+          </button>
+          <button
+            data-active={platform === 'ios'}
+            onClick={updatePlatform}
+            data-platform="ios"
+          >
+            iOS
+          </button>
+          <button
+            data-active={platform === 'android'}
+            onClick={updatePlatform}
+            data-platform="android"
+          >
+            ANDROID
+          </button>
         </div>
-        <div className='tutorial-screen'> 
-          {platform === 'mac' && <Mac/>}
-          {platform === 'windows' && <WindowsClone/>}
-          {platform === 'ios' && <IOSClone/>}
-          {platform === 'android' && <PixelClone/>}
+        <div className="tutorial-screen">
+          {platform === 'mac' && <Mac />}
+          {platform === 'windows' && <Windows11 />}
+          {platform === 'ios' && <IOSClone />}
+          {platform === 'android' && <PixelClone />}
         </div>
       </div>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/assets/SelectionPage.jsx
+++ client/src/assets/SelectionPage.jsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import "./SelectionPage.css";
 import { FaAndroid, FaWindows } from "react-icons/fa";
 import { SiIos, SiMacos } from "react-icons/si";
+import { useHistory } from "react-router-dom";
 
 const SelectionPage = () => {
-  const [selectedIcon, setSelectedIcon] = useState(null);
+  const [selectedIcon, setSelectedIcon] = useState("");
   const [showStartButton, setShowStartButton] = useState(false);
+  const history = useHistory();
 
   const handleIconClick = (icon) => {
     setSelectedIcon(icon);
@@ -14,6 +16,9 @@ const SelectionPage = () => {
 
   const handleStartButtonClick = () => {
     console.log("Start button clicked for", selectedIcon);
+    // Perform sign-in logic
+    // If sign-in is successful, navigate to the main content
+    history.push("/main");
   };
 
   return (
@@ -25,46 +30,46 @@ const SelectionPage = () => {
       <div className={`button-container ${selectedIcon ? "collapsed" : ""}`}>
         {!selectedIcon ? (
           <>
-            <button className="button" onClick={() => handleIconClick("android")}>
-              <FaAndroid className="icon" />
+            <button className="button-os" onClick={() => handleIconClick("android")}>
+              <FaAndroid className="icon" size={64} />
               <span className="icon-text">Android</span>
             </button>
-            <button className="button" onClick={() => handleIconClick("windows")}>
-              <FaWindows className="icon" />
+            <button className="button-os" onClick={() => handleIconClick("windows")}>
+              <FaWindows className="icon" size={64} />
               <span className="icon-text">Windows</span>
             </button>
-            <button className="button" onClick={() => handleIconClick("ios")}>
-              <SiIos className="icon" />
+            <button className="button-os" onClick={() => handleIconClick("ios")}>
+              <SiIos className="icon" size={64} />
               <span className="icon-text">iPhone</span>
             </button>
-            <button className="button" onClick={() => handleIconClick("macos")}>
-              <SiMacos className="icon" />
+            <button className="button-os" onClick={() => handleIconClick("macos")}>
+              <SiMacos className="icon" size={64} />
               <span className="icon-text">macOS</span>
             </button>
           </>
         ) : (
-          <button className="button selected" onClick={handleStartButtonClick}>
+          <button className="selected-button" onClick={handleStartButtonClick}>
             {selectedIcon === "android" && (
               <>
-                <FaAndroid className="icon" />
+                <FaAndroid className="icon" size={64} />
                 <span className="icon-text">Android</span>
               </>
             )}
             {selectedIcon === "windows" && (
               <>
-                <FaWindows className="icon" />
+                <FaWindows className="icon" size={64} />
                 <span className="icon-text">Windows</span>
               </>
             )}
             {selectedIcon === "ios" && (
               <>
-                <SiIos className="icon" />
+                <SiIos className="icon" size={64} />
                 <span className="icon-text">iPhone</span>
               </>
             )}
             {selectedIcon === "macos" && (
               <>
-                <SiMacos className="icon" />
+                <SiMacos className="icon" size={64} />
                 <span className="icon-text">macOS</span>
               </>
             )}
@@ -72,9 +77,9 @@ const SelectionPage = () => {
         )}
       </div>
       {showStartButton && (
-        <button className="start-button" onClick={handleStartButtonClick}>
-          Start
-        </button>
+        <>
+          <SignInForm />
+        </>
       )}
     </div>
   );
